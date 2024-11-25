3c823b400660f17d711f493d76247831b5c1503e
oosuhada <woosu91@gmail.com>
2024-11-25 21:36:57 +0900

    [organization_WS] edit json_utils.dart file for connectivity between files and json data

--- lib/utils/json_utils.dart
+++ lib/utils/json_utils.dart
@@ -1,9 +1,10 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:path_provider/path_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+// JSON 파일에서 데이터를 로드하는 함수
 Future<List<dynamic>> loadJsonData(String path, name) async {
   loadJsonData(path, name);
   final String response = await rootBundle.loadString(path);
@@ -11,9 +12,10 @@ Future<List<dynamic>> loadJsonData(String path, name) async {
   return data[name];
 }
 
+// 사용자 정보를 로드하는 함수
 Future<Map<String, dynamic>> loadUserData() async {
   final directory = await getApplicationDocumentsDirectory();
-  final file = File('${directory.path}/users.json');
+  final file = File('${directory.path}/userInfo.json');
   if (await file.exists()) {
     String contents = await file.readAsString();
     return json.decode(contents);
@@ -21,12 +23,14 @@ Future<Map<String, dynamic>> loadUserData() async {
   return {"users": []};
 }
 
+// 사용자 정보를 저장하는 함수
 Future<void> saveUserData(Map<String, dynamic> userData) async {
   final directory = await getApplicationDocumentsDirectory();
-  final file = File('${directory.path}/users.json');
+  final file = File('${directory.path}/userInfo.json');
   await file.writeAsString(json.encode(userData));
 }
 
+// 새로운 사용자 ID를 생성하는 함수
 String generateUserId(List<dynamic> users) {
   int maxId = users.isEmpty
       ? 0
@@ -36,191 +40,194 @@ String generateUserId(List<dynamic> users) {
   return 'user${(maxId + 1).toString().padLeft(6, '0')}';
 }
 
-// Future<List<dynamic>> getPostsForUser(String userId, String postType) async {
-//   final usersData = await loadJsonData('assets/data/users.json');
-//   final postsData = await loadJsonData('assets/data/posts.json');
+// 특정 사용자의 게시물을 가져오는 함수
+Future<List<dynamic>> getPostsForUser(String userId, String postType) async {
+  final usersData = await loadJsonData('assets/data/userInfo.json', "userInfo");
+  final postsData =
+      await loadJsonData('assets/data/productInfo.json', "productInfo");
 
-//   final user =
-//       usersData['users'].firstWhere((user) => user['userId'] == userId);
-//   final postIds = user['posts'][postType];
+  final user = usersData.firstWhere((user) => user['userId'] == userId);
+  final postIds = user['posts'][postType];
 
-//   return postIds
-//       .map((postId) =>
-//           postsData['posts'].firstWhere((post) => post['postId'] == postId))
-//       .toList();
-// }
+  return postIds
+      .map((postId) => postsData.firstWhere((post) => post['postId'] == postId))
+      .toList();
+}
 
-// Future<void> addNewUser(String nickname, String profileImagePath) async {
-//   final userData = await loadUserData();
-//   final users = userData['users'] as List<dynamic>;
+// 새로운 사용자를 추가하는 함수
+Future<void> addNewUser(String nickname, String profileImagePath) async {
+  final userData = await loadUserData();
+  final users = userData['users'] as List<dynamic>;
 
-//   final newUserId = generateUserId(users);
-//   users.add({
-//     "userId": newUserId,
-//     "nickname": nickname,
-//     "profileImage": profileImagePath,
-//     "preferences": {"country": "서울, 대한민국", "language": "한국어"},
-//     "posts": {"selling": [], "sold": [], "purchased": []},
-//     "wishlist": [],
-//     "chatRooms": [],
-//     "rating": 0.0,
-//     "joinDate": DateTime.now().toIso8601String(),
-//     "lastActive": DateTime.now().toIso8601String()
-//   });
+  final newUserId = generateUserId(users);
+  users.add({
+    "userId": newUserId,
+    "nickname": nickname,
+    "profileImage": profileImagePath,
+    "city": {
+      "homeCity": "서울, 대한민국",
+      "selectedCity": "서울, 대한민국",
+      "language": "한국어"
+    },
+    "posts": {"selling": [], "sold": [], "purchased": []},
+    "wishlist": [],
+    "chatRooms": [],
+    "rating": 0.0,
+    "joinDate": DateTime.now().toIso8601String(),
+    "lastActive": DateTime.now().toIso8601String()
+  });
 
-//   await saveUserData(userData);
-// }
+  await saveUserData(userData);
+}
 
-// //게시물 데이터 저장 함수
-// Future<void> savePostData(Map<String, dynamic> postData) async {
-//   final directory = await getApplicationDocumentsDirectory();
-//   final file = File('${directory.path}/posts.json');
-//   await file.writeAsString(json.encode(postData));
-// }
+// 닉네임으로 userId를 조회하는 함수
+Future<String?> getUserIdByNickname(String nickname) async {
+  final userData = await loadUserData();
+  final user = userData['users']
+      .firstWhere((user) => user['nickname'] == nickname, orElse: () => null);
+  return user?['userId'];
+}
 
-// //특정 게시물 가져오기 함수
-// Future<Map<String, dynamic>?> getPostById(String postId) async {
-//   final postsData = await loadJsonData('assets/data/posts.json');
-//   var post = postsData['posts']
-//       .firstWhere((post) => post['postId'] == postId, orElse: () => null);
+// 현재 로그인된 사용자의 ID를 가져오는 함수
+Future<String?> getCurrentUserId() async {
+  final prefs = await SharedPreferences.getInstance();
+  final nickname = prefs.getString('nickname');
+  if (nickname != null) {
+    return getUserIdByNickname(nickname);
+  }
+  return null;
+}
 
-//   if (post != null && post['images'] != null) {
-//     post['images'] = (post['images'] as List).map((imagePath) {
-//       return normalizeImagePath(imagePath);
-//     }).toList();
-//   }
+// 게시물 데이터를 저장하는 함수
+Future<void> savePostData(postData) async {
+  final directory = await getApplicationDocumentsDirectory();
+  final file = File('${directory.path}/productInfo.json');
+  await file.writeAsString(json.encode(postData));
+}
 
-//   return post;
-// }
+// 특정 게시물을 ID로 가져오는 함수
+Future<Map<String, dynamic>?> getPostById(String postId) async {
+  final postsData =
+      await loadJsonData('assets/data/productInfo.json', "productInfo");
+  var post = postsData.firstWhere((post) => post['postId'] == postId,
+      orElse: () => null);
 
-// //사용자 정보 업데이트 함수
-// Future<void> updateUserInfo(
-//     String userId, Map<String, dynamic> updatedInfo) async {
-//   final userData = await loadUserData();
-//   final users = userData['users'] as List<dynamic>;
-//   final userIndex = users.indexWhere((user) => user['userId'] == userId);
+  if (post != null && post['images'] != null) {
+    post['images'] = (post['images'] as List).map((imagePath) {
+      return normalizeImagePath(imagePath);
+    }).toList();
+  }
 
-//   if (userIndex != -1) {
-//     users[userIndex] = {...users[userIndex], ...updatedInfo};
-//     await saveUserData(userData);
-//   }
-// }
+  return post;
+}
 
-// //새 게시물 추가 함수
-// Future<void> addNewPost(String userId, Map<String, dynamic> postData) async {
-//   // 이미지 경로 정규화
-//   if (postData['images'] != null) {
-//     postData['images'] = (postData['images'] as List).map((imagePath) {
-//       return normalizeImagePath(imagePath.toString());
-//     }).toList();
-//   }
+// 새로운 게시물을 추가하는 함수
+Future<void> addNewPost(String userId, Map<String, dynamic> postData) async {
+  // 이미지 경로 정규화
+  if (postData['images'] != null) {
+    postData['images'] = (postData['images'] as List).map((imagePath) {
+      return normalizeImagePath(imagePath.toString());
+    }).toList();
+  }
 
-//   final userData = await loadUserData();
-//   final users = userData['users'] as List<dynamic>;
-//   final userIndex = users.indexWhere((user) => user['userId'] == userId);
+  final userData = await loadUserData();
+  final users = userData['users'] as List<dynamic>;
+  final userIndex = users.indexWhere((user) => user['userId'] == userId);
 
-//   if (userIndex != -1) {
-//     // 기존의 selling 목록에 새 postId 추가
-//     users[userIndex]['posts']['selling'].add(postData['postId']);
-//     await saveUserData(userData);
+  if (userIndex != -1) {
+    // 기존의 selling 목록에 새 postId 추가
+    users[userIndex]['posts']['selling'].add(postData['postId']);
+    await saveUserData(userData);
 
-//     // posts.json에 새 게시물 추가
-//     final postsData = await loadJsonData('assets/data/posts.json');
-//     postsData['posts'].add(postData);
-//     await savePostData(postsData);
-//   }
-// }
+    // productInfo.json에 새 게시물 추가
+    final postsData =
+        await loadJsonData('assets/data/productInfo.json', "productInfo");
+    postsData.add(postData);
+    await savePostData(postsData);
+  }
+}
 
-// // 이미지 경로 정규화를 위한 헬퍼 함수 추가
-// String normalizeImagePath(String path) {
-//   // 기존 경로에서 파일명만 추출
-//   String fileName = path.split('/').last;
-//   // 새로운 경로로 조합
-//   return 'assets/data/posts_images/$fileName';
-// }
+// 판매 중인 아이템 로드
+Future<List<dynamic>> loadSellingItems(String userId) async {
+  final userData = await loadUserData();
+  final user = userData['users'].firstWhere((user) => user['userId'] == userId);
+  return getPostsForUser(userId, 'selling');
+}
 
-// //게시물 상태 변경 함수
-// Future<void> changePostStatus(
-//     String userId, String postId, String newStatus) async {
-//   final userData = await loadUserData();
-//   final users = userData['users'] as List<dynamic>;
-//   final userIndex = users.indexWhere((user) => user['userId'] == userId);
+// 판매 완료된 아이템 로드
+Future<List<dynamic>> loadSoldItems(String userId) async {
+  final userData = await loadUserData();
+  final user = userData['users'].firstWhere((user) => user['userId'] == userId);
+  return getPostsForUser(userId, 'sold');
+}
 
-//   if (userIndex != -1) {
-//     final user = users[userIndex];
-//     user['posts']['selling'].remove(postId);
-//     user['posts'][newStatus].add(postId);
-//     await saveUserData(userData);
+// 구매한 아이템 로드
+Future<List<dynamic>> loadPurchasedItems(String userId) async {
+  final userData = await loadUserData();
+  final user = userData['users'].firstWhere((user) => user['userId'] == userId);
+  return getPostsForUser(userId, 'purchased');
+}
 
-//     final postsData = await loadJsonData('assets/data/posts.json');
-//     final postIndex =
-//         postsData['posts'].indexWhere((post) => post['postId'] == postId);
-//     if (postIndex != -1) {
-//       postsData['posts'][postIndex]['status'] = newStatus;
-//       await savePostData(postsData);
-//     }
-//   }
-// }
+// 위시리스트 아이템 로드
+Future<List<dynamic>> loadWishlistItems(String userId) async {
+  final userData = await loadUserData();
+  final user = userData['users'].firstWhere((user) => user['userId'] == userId);
+  final wishlistIds = user['wishlist'] ?? [];
 
-// // 사용자의 구매 항목 가져오기
-// Future<List<dynamic>> getPurchasedItems(String userId) async {
-//   return getPostsForUser(userId, 'purchased');
-// }
+  final postsData =
+      await loadJsonData('assets/data/productInfo.json', "productInfo");
+  return wishlistIds
+      .map((postId) => postsData.firstWhere((post) => post['postId'] == postId))
+      .toList();
+}
 
-// // 사용자의 판매중인 항목 가져오기
-// Future<List<dynamic>> getSellingItems(String userId) async {
-//   return getPostsForUser(userId, 'selling');
-// }
+// 위시리스트에서 아이템 제거
+Future<void> removeFromWishlist(String userId, String postId) async {
+  final userData = await loadUserData();
+  final users = userData['users'] as List;
+  final userIndex = users.indexWhere((user) => user['userId'] == userId);
 
-// // 사용자의 판매 완료 항목 가져오기
-// Future<List<dynamic>> getSoldItems(String userId) async {
-//   return getPostsForUser(userId, 'sold');
-// }
+  if (userIndex != -1) {
+    List wishlist = users[userIndex]['wishlist'] as List;
+    wishlist.remove(postId);
+    await saveUserData(userData);
+  }
+}
 
-// // 사용자의 구매 항목 추가하기
-// Future<void> addPurchasedItem(String userId, String postId) async {
-//   await _addItemToUserPosts(userId, postId, 'purchased');
-// }
+// 이미지 경로 정규화를 위한 헬퍼 함수 추가
+String normalizeImagePath(String path) {
+  // 기존 경로에서 파일명만 추출
+  String fileName = path.split('/').last;
+  // 새로운 경로로 조합
+  return 'assets/data/posts_images/$fileName';
+}
 
-// // 사용자의 판매중인 항목 추가하기
-// Future<void> addSellingItem(String userId, String postId) async {
-//   await _addItemToUserPosts(userId, postId, 'selling');
-// }
+// 채팅 정보를 로드하는 함수 추가
+Future<Map<String, dynamic>> loadChattingInfo() async {
+  final directory = await getApplicationDocumentsDirectory();
+  final file = File('${directory.path}/chattingInfo.json');
 
-// // 사용자의 판매 완료 항목 추가하기
-// Future<void> addSoldItem(String userId, String postId) async {
-//   await _addItemToUserPosts(userId, postId, 'sold');
-// }
+  if (await file.exists()) {
+    String contents = await file.readAsString();
+    return json.decode(contents);
+  }
 
-// // 사용자의 게시물에 항목을 추가하는 헬퍼 함수
-// Future<void> _addItemToUserPosts(
-//     String userId, String postId, String postType) async {
-//   final userData = await loadUserData();
-//   final users = userData['users'] as List<dynamic>;
-//   final userIndex = users.indexWhere((user) => user['userId'] == userId);
+  return {"chatting": []};
+}
 
-//   if (userIndex != -1) {
-//     users[userIndex]['posts'][postType].add(postId);
-//     await saveUserData(userData);
-//   }
-// }
+// 사용자 채팅 정보를 업데이트하는 함수 추가
+Future<void> updateChattingInfo(
+    String userId, Map<String, dynamic> chatUpdate) async {
+  final chattingInfo = await loadChattingInfo();
+  final chattingUsers = chattingInfo['chatting'] as List<dynamic>;
+  final chatIndex =
+      chattingUsers.indexWhere((chatUser) => chatUser['userId'] == userId);
 
-// // 사용자의 게시물에서 항목 제거하기
-// Future<void> removeItemFromUserPosts(
-//     String userId, String postId, String postType) async {
-//   final userData = await loadUserData();
-//   final users = userData['users'] as List<dynamic>;
-//   final userIndex = users.indexWhere((user) => user['userId'] == userId);
-
-//   if (userIndex != -1) {
-//     users[userIndex]['posts'][postType].remove(postId);
-//     await saveUserData(userData);
-//   }
-// }
-
-// // 항목을 한 게시물 유형에서 다른 유형으로 이동하기 (예: 판매중에서 판매완료로)
-// Future<void> moveItemBetweenPostTypes(
-//     String userId, String postId, String fromType, String toType) async {
-//   await removeItemFromUserPosts(userId, postId, fromType);
-//   await _addItemToUserPosts(userId, postId, toType);
-// }
+  if (chatIndex != -1) {
+    chattingUsers[chatIndex] = {...chattingUsers[chatIndex], ...chatUpdate};
+    // Save updated chat info back to the JSON file.
+    final directory = await getApplicationDocumentsDirectory();
+    final file = File('${directory.path}/chattingInfo.json');
+    await file.writeAsString(json.encode(chattingInfo));
+  }
+}
